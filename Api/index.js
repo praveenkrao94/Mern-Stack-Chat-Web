6c7e05f3698926591eaa60a948dc7e28f578ca75
praveenkr0812@gmail.com <praveen.mindnerves@gmail.com>
2023-06-29 17:16:09 +0530

    resgister

--- Api/index.js
+++ Api/index.js
@@ -1,16 +1,40 @@
 const express = require('express');
+
 const mongoose = require('mongoose');
+
+const cors = require('cors');
+
 const app = express();
+
+const User = require('./model/User');
 
 require('dotenv').config();
 
-app.use(express.urlencoded({extended:true}))
 
-app.use(express.json())
 
-// ------------------------------------------------------------------------------------------------------------
+const CookieParser = require('cookie-parser')
 
-mongoose.connect(process.env.MONGO_URL)
+const jwtSecret = process.env.JWT_SECRET;
+
+const jwt = require('jsonwebtoken');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+
+app.use(CookieParser());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log('Connected to MongoDB successfully');
   })
@@ -18,16 +42,27 @@ mongoose.connect(process.env.MONGO_URL)
     console.error('Failed to connect to MongoDB:', error);
   });
 
+// ---------------------------------------
 
-//-------------------------------------------------------------------------------------------------------------
-
-app.get('/test', (req, res) => {
-  res.json('test ok');
-});
-
-app.post('/register', (req, res) => {
-  const { email, password } = req.body;
-  res.json();
+// Register a new user
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const createdUser = await User.create({ username, password });
+    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
+      if (err) {
+        console.error('Failed to create JWT token:', err);
+        res.status(500).json('Internal server error');
+      } else {
+        res.cookie('token', token).status(201).json({
+          _id:CreatedUser._id
+        });
+      }
+    });
+  } catch (err) {
+    console.error('Failed to register user:', err);
+    res.status(500).json('Internal server error');
+  }
 });
 
 app.listen(4040, () => {
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Register from './register/Register'
+import axios from 'axios'
 
 function App() {
+
+  axios.defaults.baseURL = "http://localhost:4040"
+  axios.defaults.withCredentials = true
+
   return (
     <Register/>
   )
